e0ec416e960859636e9193808253c6a73294fa58
kitasensei <kazuya.dct@gmail.com>
2025-09-17 20:14:35 +0700

    変更

--- src/03_7seg_LED_test.ino
+++ src/03_7seg_LED_test.ino
@@ -1,25 +1,24 @@
 //ARDINO NANO
-//TM1637接続ピン番号:DIOpin=2(デジタルピン2) CLKpin=3（デジタルピン3）
+//TM1637接続ピン番号:KIDIOpin=2(デジタルピン2) KICLKpin=3（デジタルピン3）
 //I2c通信ではないため、専用ピンがそれぞれに必要
-//DS3231接続ピン番号:DIOpin=A4 CLKpin=A5（ハードI2cピンを使用）このスケッチでは未使用
+//DS3231接続ピン番号:KIDIOpin=A4 KICLKpin=A5（ハードI2cピンを使用）このスケッチでは未使用
 #include <Arduino.h> 
 // #include <TM1637Display.h>
-#define CLK 5
-#define DIO 6
-//TM1637Display display(CLK, DIO);
+#define KICLK 5
+#define KIDIO 6
+//TM1637Display display(KICLK, KIDIO);
 //#include <Wire.h>
 
 void setup(){
   //Wire.begin();      //マスターで接続
   Serial.begin(9600);//シリアル通信スタート
   //display.setBrightness(3);//0~7 7が一番明るい
-  pinMode(CLK,OUTPUT);
-  pinMode(DIO,OUTPUT);
-  delayMicroseconds(5);
-  digitalWrite(CLK,HIGH);
-  digitalWrite(DIO,HIGH);
+  pinMode(KICLK,OUTPUT);
+  pinMode(KIDIO,OUTPUT);
+  delayMicroseconds(2);
+  digitalWrite(KICLK,HIGH);
+  digitalWrite(KIDIO,HIGH);
   delay(500);
-  Serial.println("setup OK");
 }
 
 
@@ -28,338 +27,331 @@ void loop() {
 
 //START signal
 //--------------------
-digitalWrite(CLK,HIGH);
-digitalWrite(DIO,HIGH);
-delayMicroseconds(5);
-digitalWrite(DIO,LOW);//DIO LOW
-Serial.println("1st start signal OK");
+digitalWrite(KICLK,HIGH);
+digitalWrite(KIDIO,HIGH);
+delayMicroseconds(2);
+digitalWrite(KIDIO,LOW);//KIDIO LOW
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
 //--------------------
 
 //command 1 (DATA command) LSB 1st 0x 0100 0000
   
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B0 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse1
-delayMicroseconds(5);
+digitalWrite(KIDIO,LOW);//B0 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse1
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);  
-digitalWrite(DIO,LOW);//B1 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse2
-delayMicroseconds(5);
+digitalWrite(KIDIO,LOW);//B1 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse2
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW); 
-digitalWrite(DIO,LOW);//B2 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse3
-delayMicroseconds(5);
-  
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B3 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse4
-delayMicroseconds(5);
+digitalWrite(KIDIO,LOW);//B2 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse3
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);  
-digitalWrite(DIO,LOW);//B4 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse5
-delayMicroseconds(5);
-  
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B5 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse6
-delayMicroseconds(5);
+digitalWrite(KIDIO,LOW);//B3 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse4
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B6 1
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse7
-delayMicroseconds(5);
+digitalWrite(KIDIO,LOW);//B4 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse5
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B7 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse8
+digitalWrite(KIDIO,LOW);//B5 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse6
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
 
-//ACK rev1.6
+digitalWrite(KIDIO,HIGH);//B6 1
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse7
+delayMicroseconds(2);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
+
+digitalWrite(KIDIO,LOW);//B7 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse8
+delayMicroseconds(2);
+
+//ACK rev1.9
 //--------------------  
-digitalWrite(CLK,LOW); // まずCLKをLOWにして合図
-pinMode(DIO, INPUT); // その後、DIOを入力モードに切り替える
-delayMicroseconds(10);//ACK 信号待ち時間
-if (digitalRead(DIO) != 0 ) {
-  Serial.println("ACK fails");
+digitalWrite(KICLK,LOW); // まずKICLKをLOWにして合図
+digitalWrite(KIDIO,LOW);
+pinMode(KIDIO, INPUT); // その後、KIDIOを入力モードに切り替える
+delayMicroseconds(2);//ACK 信号待ち時間
+if (digitalRead(KIDIO) == HIGH ) {
+Serial.println("ACK FALES 1"); 
 }
 else{
-  pinMode(DIO,OUTPUT);
-  delayMicroseconds(10);
-  digitalWrite(CLK,HIGH);
-  delayMicroseconds(5);
-  digitalWrite(CLK,LOW);
-  delayMicroseconds(5);
+  pinMode(KIDIO,OUTPUT);
+  delayMicroseconds(2);
+  digitalWrite(KICLK,HIGH);
+  delayMicroseconds(2);
+  digitalWrite(KICLK,LOW);
+  delayMicroseconds(2);
 }
 //--------------------  
-Serial.println("command1 OK");
 
 //STOP signal
 //--------------------
-digitalWrite(CLK,LOW);
-delayMicroseconds(5);
-digitalWrite(DIO,LOW);
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);
-delayMicroseconds(5);
-digitalWrite(DIO,HIGH);
+digitalWrite(KICLK,HIGH);
+digitalWrite(KIDIO,HIGH);
 //--------------------
 
 //START signal
 //--------------------
-digitalWrite(CLK,HIGH);
-digitalWrite(DIO,HIGH);
-delayMicroseconds(5);
-digitalWrite(DIO,LOW);//DIO LOW
-Serial.println("1st start signal OK");
+digitalWrite(KIDIO,LOW);
+digitalWrite(KICLK,LOW);
+
 //--------------------
 
 //command 2 (Start ADDRESS) LSB 1st 0x 1100 0000
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B0 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse1
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B0 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse1
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B1 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse2
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B1 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse2
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B2 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse3
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B2 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse3
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B3 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse4
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B3 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse4
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B4 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse5
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B4 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse5
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B5 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse6
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B5 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse6
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B6 1
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse7
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,HIGH);//B6 1
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse7
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B7 1
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,HIGH);//B7 1
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse
 
-//ACK rev1.6
+//ACK rev1.8
 //--------------------  
-digitalWrite(CLK,LOW); // まずCLKをLOWにして合図
-pinMode(DIO, INPUT); // その後、DIOを入力モードに切り替える
-delayMicroseconds(10);//ACK 信号待ち時間
-if (digitalRead(DIO) != 0 ) {
-  Serial.println("ACK fails");
+digitalWrite(KICLK,LOW); // まずKICLKをLOWにして合図
+pinMode(KIDIO, INPUT); // その後、KIDIOを入力モードに切り替える
+delayMicroseconds(2);//ACK 信号待ち時間
+if (digitalRead(KIDIO) == HIGH ) {
+Serial.println("ACK FALES 2"); 
 }
 else{
-  pinMode(DIO,OUTPUT);
-  delayMicroseconds(10);
-  digitalWrite(CLK,HIGH);
-  delayMicroseconds(5);
-  digitalWrite(CLK,LOW);
-  delayMicroseconds(5);
+  pinMode(KIDIO,OUTPUT);
+  digitalWrite(KICLK,HIGH);
+  digitalWrite(KICLK,LOW);
 }
 //--------------------  
-Serial.println("command2 OK");
 
 
 //7seg DATA 7 0x 0000 0100
 
-digitalWrite(CLK,LOW);  
-digitalWrite(DIO,LOW);//B0 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse1
-delayMicroseconds(5);
-
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B1 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse2
+digitalWrite(KICLK,LOW);  
+digitalWrite(KIDIO,LOW);//B0 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse1
 delayMicroseconds(5);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B2 1
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse3
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B1 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse2
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B3 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse4
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,HIGH);//B2 1
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse3
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B4 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse5
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B3 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse4
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B5 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse6
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B4 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse5
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B6 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse7
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B5 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse6
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B7 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse8
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B6 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse7
+delayMicroseconds(2);
 
-//ACK rev1.6
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B7 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse8
+
+//ACK rev1.7
 //--------------------  
-digitalWrite(CLK,LOW); // まずCLKをLOWにして合図
-pinMode(DIO, INPUT); // その後、DIOを入力モードに切り替える
-delayMicroseconds(10);//ACK 信号待ち時間
-if (digitalRead(DIO) != 0 ) {
-  Serial.println("ACK fails");
+digitalWrite(KICLK,LOW); // まずKICLKをLOWにして合図
+pinMode(KIDIO, INPUT); // その後、KIDIOを入力モードに切り替える
+delayMicroseconds(5);//ACK 信号待ち時間
+if (digitalRead(KIDIO) == HIGH ) {
 }
 else{
-  pinMode(DIO,OUTPUT);
-  delayMicroseconds(10);
-  digitalWrite(CLK,HIGH);
+  pinMode(KIDIO,OUTPUT);
   delayMicroseconds(5);
-  digitalWrite(CLK,LOW);
-  delayMicroseconds(5);
+  digitalWrite(KICLK,HIGH);
+  delayMicroseconds(2);
+  digitalWrite(KICLK,LOW);
+  delayMicroseconds(2);
 }
 //--------------------  
 
 //STOP signal
 //--------------------
-digitalWrite(CLK,LOW);
-delayMicroseconds(5);
-digitalWrite(DIO,LOW);
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);
-delayMicroseconds(5);
-digitalWrite(DIO,HIGH);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
+digitalWrite(KIDIO,LOW);
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);
+delayMicroseconds(2);
+digitalWrite(KIDIO,HIGH);
 //--------------------
 
 //START signal
 //--------------------
-digitalWrite(CLK,HIGH);
-digitalWrite(DIO,HIGH);
-delayMicroseconds(5);
-digitalWrite(DIO,LOW);//DIO LOW
-Serial.println("1st start signal OK");
+digitalWrite(KICLK,HIGH);
+digitalWrite(KIDIO,HIGH);
+delayMicroseconds(2);
+digitalWrite(KIDIO,LOW);//KIDIO LOW
 //--------------------  
 
-Serial.println("7seg data OK");
 
 //command 3 (0x 0100 1010) displayON LEVEL3
   
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B0 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse1
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B0 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse1
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B1 
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse2
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,HIGH);//B1 
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse2
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B2 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse3
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B2 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse3
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B3 1
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse4
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,HIGH);//B3 1
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse4
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B4 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse5
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B4 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse5
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,LOW);//B5 0
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse6
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,LOW);//B5 0
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse6
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B6 1
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse7
-delayMicroseconds(5);
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,HIGH);//B6 1
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse7
+delayMicroseconds(2);
 
-digitalWrite(CLK,LOW);
-digitalWrite(DIO,HIGH);//B7 1
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);//CLK pulse8
+digitalWrite(KICLK,LOW);
+digitalWrite(KIDIO,HIGH);//B7 1
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);//KICLK pulse8
 
-//ACK rev1.6
+//ACK rev1.7
 //--------------------  
-digitalWrite(CLK,LOW); // まずCLKをLOWにして合図
-pinMode(DIO, INPUT); // その後、DIOを入力モードに切り替える
-delayMicroseconds(10);//ACK 信号待ち時間
-if (digitalRead(DIO) != 0 ) {
-  Serial.println("ACK fails");
+digitalWrite(KICLK,LOW); // まずKICLKをLOWにして合図
+pinMode(KIDIO, INPUT); // その後、KIDIOを入力モードに切り替える
+delayMicroseconds(5);//ACK 信号待ち時間
+if (digitalRead(KIDIO) == HIGH ) {
 }
 else{
-  pinMode(DIO,OUTPUT);
-  delayMicroseconds(10);
-  digitalWrite(CLK,HIGH);
+  pinMode(KIDIO,OUTPUT);
   delayMicroseconds(5);
-  digitalWrite(CLK,LOW);
-  delayMicroseconds(5);
+  digitalWrite(KICLK,HIGH);
+  delayMicroseconds(2);
+  digitalWrite(KICLK,LOW);
+  delayMicroseconds(2);
 }
 //--------------------  
 
 //STOP signal
 //--------------------
-digitalWrite(CLK,LOW);
-delayMicroseconds(5);
-digitalWrite(DIO,LOW);
-delayMicroseconds(5);
-digitalWrite(CLK,HIGH);
-delayMicroseconds(5);
-digitalWrite(DIO,HIGH);
+digitalWrite(KICLK,LOW);
+delayMicroseconds(2);
+digitalWrite(KIDIO,LOW);
+delayMicroseconds(2);
+digitalWrite(KICLK,HIGH);
+delayMicroseconds(2);
+digitalWrite(KIDIO,HIGH);
 //--------------------
 
-Serial.println("final command OK");
 
 delay(1000);
 }
